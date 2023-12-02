7c3473b3eaa20e1fe61c3a87e518dc4502fb19f8
Tobias Trawe <tobias.trawe@gmail.com>
2023-12-02 02:53:02 +0100

    fixed handling of session

--- src/main/java/se/trawe/aoc/InputGetter.java
+++ src/main/java/se/trawe/aoc/InputGetter.java
@@ -1,19 +1,29 @@
 package se.trawe.aoc;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
+import java.util.Objects;
 
 public class InputGetter {
+
+    private static String session = getSession();
+
+    static String getSession() {
+        try {
+            return session = new BufferedReader(new InputStreamReader(Objects.requireNonNull(InputGetter.class.getClassLoader().getResourceAsStream("session.txt")))).readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static List<String> getUrlContents(int i) {
         try {
             URL url = new URI("https://adventofcode.com/2023/day/" + i + "/input").toURL();
             URLConnection urlConnection = url.openConnection();
-            urlConnection.setRequestProperty("Cookie", "session=53616c7465645f5fca027570238afda036f835287a7e868c52a518d38ab6ec0e6f6805e5f495e623d4d4c2566943da86f72a84b22f6622ea96a8f0e76ad96114");
+            urlConnection.setRequestProperty("Cookie", "session=" + session);
             return new BufferedReader(new InputStreamReader(urlConnection.getInputStream())).lines().toList();
         } catch (Exception e) {
             throw new RuntimeException(e);
