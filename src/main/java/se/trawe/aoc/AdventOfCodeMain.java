9a9b7c770a5af05499d9c71e38fd60dbe495ee03
Tobias Trawe <tobias.trawe@gmail.com>
2023-12-02 02:23:46 +0100

    25 days in the for loop

--- src/main/java/se/trawe/aoc/AdventOfCodeMain.java
+++ src/main/java/se/trawe/aoc/AdventOfCodeMain.java
@@ -3,7 +3,7 @@ package se.trawe.aoc;
 public class AdventOfCodeMain {
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 24; i++) {
+        for (int i = 1; i <= 25; i++) {
             try {
                 Class<?> c = Class.forName("se.trawe.aoc.days.Day" + i);
                 Task.register((Task) c.getDeclaredMethod("getInstance", null).invoke(null, null));
