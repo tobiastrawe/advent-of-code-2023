4fe23747c21c717b07ad2ee983e19e3c42b1f8a0
Tobias Trawe <tobias.trawe@gmail.com>
2023-12-11 12:11:54 +0100

    day 11

--- src/main/java/se/trawe/aoc/days/Day11.java
+++ src/main/java/se/trawe/aoc/days/Day11.java
@@ -1,5 +1,12 @@
 package se.trawe.aoc.days;
 
+import se.trawe.aoc.util.ArrayUtil;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.List;
 
 public class Day11 extends Task {
@@ -24,13 +31,66 @@ public class Day11 extends Task {
 
     @Override
     protected String runTaskOne(List<String> input) {
-        return "no result";
+        char[][] universe = getUniverseScreen(input);
+        List<Integer> expandX = generateIndices(universe);
+        List<Integer> expandY = generateIndices(ArrayUtil.rotateCW(universe));
+        Set<Point> coordinates = ArrayUtil.getCoordinatesForCharacter(universe, '#');
+        var result = 0L;
+        for (Point p : coordinates) {
+            result += coordinates.stream().filter(c -> !c.equals(p)).mapToLong(c -> manhattanDistanceUniverse(p, c, expandY, expandX, 1)).sum();
+        }
+        return String.valueOf(result/2);
+    }
+
+    private static List<Integer> generateIndices(char[][] universe) {
+        List<Integer> expandX = new ArrayList<>();
+        for (int y = 0; y < universe.length; y++) {
+            var row = universe[y];
+            if (!Arrays.toString(row).contains("#")) {
+                expandX.add(y);
+            }
+        }
+        return expandX;
     }
 
     @Override
     protected String runTaskTwo(List<String> input) {
-        return "no result";
+        char[][] universe = getUniverseScreen(input);
+        List<Integer> expandX = generateIndices(universe);
+        List<Integer> expandY = generateIndices(ArrayUtil.rotateCW(universe));
+        Set<Point> coordinates = ArrayUtil.getCoordinatesForCharacter(universe, '#');
+        var result = 0L;
+        for (Point p : coordinates) {
+            result += coordinates.stream().filter(c -> !c.equals(p)).mapToLong(c ->
+                    manhattanDistanceUniverse(p, c, expandY, expandX, 1000000-1)).sum();
+        }
+        return String.valueOf(result/2);
     }
 
+    private static char[][] getUniverseScreen(List<String> input) {
+        char[][] universe = new char[input.size()][input.get(0).length()];
+        for (int y = 0; y < input.size(); y++) {
+            String row = input.get(y);
+            for (int x = 0; x < row.length(); x++) {
+                universe[y][x] = row.charAt(x);
+            }
+        }
+        return universe;
+    }
 
+    private long manhattanDistanceUniverse(Point p, Point c, List<Integer> expandY, List<Integer> expandX, long extraSteps) {
+        return (Math.abs(p.x - c.x) + (numbersIntersecting(p.x, c.x, expandY) * extraSteps)) + (Math.abs(p.y - c.y) + (numbersIntersecting(p.y, c.y, expandX) * extraSteps));
+    }
+
+    private int numbersIntersecting(int n1, int n2, List<Integer> numbers) {
+        var result = 0;
+        var from = Math.min(n1, n2);
+        var to = Math.max(n1, n2);
+        for (; from < to; from++) {
+            if (numbers.contains(from)) {
+                result++;
+            }
+        }
+        return result;
+    }
 }
--- src/main/java/se/trawe/aoc/util/ArrayUtil.java
+++ src/main/java/se/trawe/aoc/util/ArrayUtil.java
@@ -2,9 +2,38 @@ package se.trawe.aoc.util;
 
 import se.trawe.aoc.days.Day10;
 
+import java.awt.*;
+import java.util.HashSet;
 import java.util.Stack;
+import java.util.Set;
 
 public class ArrayUtil {
+
+    public static Set<Point> getCoordinatesForCharacter(char[][] screen, char c) {
+        Set<Point> coordinates = new HashSet<>();
+        for (int y = 0; y < screen.length; y++) {
+            var row = screen[y];
+            for (int x = 0; x < row.length; x++) {
+                if (row[x] == c) {
+                    coordinates.add(new Point(x, y));
+                }
+            }
+        }
+        return coordinates;
+    }
+
+    public static char[][] rotateCW(char[][] mat) {
+        final int M = mat.length;
+        final int N = mat[0].length;
+        char[][] ret = new char[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M - 1 - r] = mat[r][c];
+            }
+        }
+        return ret;
+    }
+
     public static void floodFill(char[][] screen, char newChar, int startX, int startY) {
         Stack<int[]> stack = new Stack<>();
         stack.push(new int[]{startX, startY});
